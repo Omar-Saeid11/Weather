82b486ac4cedb07d974c456f2fb2fccaead55467
Omar Saeid <omarsaeid1911z@gmail.com>
2024-05-03 19:45:56 +0300

    add splash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.weather"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.weather"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -39,23 +39,26 @@ android {
 }
 
 dependencies {
-    implementation("com.google.android.gms:play-services-location:21.0.1")
     val fragmentVersion = "1.6.1"
     val lottieVersion = "6.0.1"
 
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     implementation("androidx.fragment:fragment-ktx:$fragmentVersion")
-    implementation("com.squareup.okhttp3:okhttp:4.11.0")
-    implementation("com.google.code.gson:gson:2.9.0")
-    implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.android.gms:play-services-location:21.2.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
     implementation("com.airbnb.android:lottie:$lottieVersion")
+    implementation ("androidx.viewpager2:viewpager2:1.0.0")
+
+    //Splash Api
+    implementation ("androidx.core:core-splashscreen:1.0.1")
 
 }
